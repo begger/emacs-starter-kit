b4f5510f2af3dfc57120239653890eb871cc4481
jhirn <joseph.hirn@thoughtworks.com>
2010-06-11 02:11:22 -0500

    added some vendor files, they might get deleted

--- jhirn.el
+++ jhirn.el
@@ -9,8 +9,10 @@
 
 
 ;; Path fuckery 
-(setenv "PATH" (concat "/opt/local/bin" ":" (getenv "PATH")))
+(setenv "PATH" (concat "/opt/local/bin" ":" "/usr/local/mongo/bin" ":" (getenv "PATH")))
 (setq exec-path (append exec-path '("/opt/local/bin")))
+(setq exec-path (append exec-path '("/usr/local/mongo/bin")))
+
 ;;(setq magit-git-executable "/usr/local/git/bin/git")
 
 ;; Keybinding
@@ -22,5 +24,25 @@
 (global-set-key (kbd "M-z") 'undo)
 (global-set-key (kbd "M-l") 'goto-line)
 (global-set-key (kbd "TAB") 'hippie-expand)
+(global-set-key (kbd "C-S-f") 'indent-buffer)
+
 
 (pastels-color-theme)
+
+(require 'rspec-mode)
+(rspec-mode)
+(require 'rvm)
+(rvm-use-default)
+(server-start)
+
+(defun maximize-frame ()
+  (interactive)
+  (set-frame-position (selected-frame) 0 0)
+  (set-frame-size (selected-frame) 1000 1000))
+
+(set-frame-parameter (selected-frame) 'alpha '(92 70))
+
+(ansi-color-for-comint-mode-on)
+
+
+
